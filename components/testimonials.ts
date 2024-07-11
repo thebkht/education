cc62ba37cf7751b3f2443750735f5e24913b166b
Bakhtiyor Ganijon <me@bkhtdev.com>
2024-07-11 13:51:53 +0500

    feat: Add testimonials data to testimonials.ts

---
+++ components/testimonials.ts
@@ -0,0 +1,23 @@
+export const testimonials = [
+  {
+    name: "Xojimirzayev Boburjon",
+    rate: 5,
+    review:
+      "Very understanding, patient and will help you improve your English. I highly recommend this teacher.",
+    image: "/instruction-1.jpg",
+  },
+  {
+    name: "Xojimirzayev Boburjon",
+    rate: 5,
+    review:
+      "Very understanding, patient and will help you improve your English. I highly recommend this teacher.",
+    image: "/instruction-1.jpg",
+  },
+  {
+    name: "Xojimirzayev Boburjon",
+    rate: 5,
+    review:
+      "Very understanding, patient and will help you improve your English. I highly recommend this teacher.",
+    image: "/instruction-1.jpg",
+  },
+];
