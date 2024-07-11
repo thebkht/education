dd7cad126a0b23c2d887df72d0f024a6ac139391
Bakhtiyor Ganijon <me@bkhtdev.com>
2024-07-11 13:38:48 +0500

    chore: Update gap size in CoursesSection grid

--- components/courses-section.tsx
+++ components/courses-section.tsx
@@ -8,7 +8,7 @@ export function CoursesSection() {
                     <div className="flex-1 flex gap-1">
                          <h3 className="text-2xl font-bold">Kurslar</h3>
                     </div>
-                    <div className="grid grid-cols-4 gap-4">
+                    <div className="grid grid-cols-4 gap-6">
                          {
                               courses.map((course, index) => (
                                    <CourseCard key={index} course={course} />
