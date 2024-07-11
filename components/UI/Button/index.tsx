8bf8f3e84e016e0cfbd78f98235aed9011c01b6d
Bakhtiyor Ganijon <me@bkhtdev.com>
2024-07-11 16:36:39 +0500

    refactor: Update Button component styles and variants

--- components/UI/Button/index.tsx
+++ components/UI/Button/index.tsx
@@ -2,7 +2,7 @@ import { cn } from "@/lib/uitls"
 import { cva, type VariantProps } from "class-variance-authority"
 
 const buttonVariants = cva(
-     "inline-flex items-center justify-center rounded-[4px] text-sm font-bold transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+     "inline-flex items-center justify-center rounded-lg text-sm font-bold transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
      {
           variants: {
                variant: {
@@ -14,8 +14,8 @@ const buttonVariants = cva(
                },
                size: {
                     default: "px-4 py-3",
-                    sm: "px-4 py-2 text-xs",
-                    lg: "rounded-lg px-6 py-3 text-base font-medium",
+                    sm: "px-4 py-2 text-xs rounded-[4px]",
+                    lg: "px-6 py-3 text-base font-medium",
                },
           },
           defaultVariants: {
