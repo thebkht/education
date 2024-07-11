64efc9c01652b853d1da2383ae044bbb661be7e0
Bakhtiyor Ganijon <me@bkhtdev.com>
2024-07-11 10:53:31 +0500

    chore: Update global CSS and Tailwind config for color scheme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,23 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        secondary: "hsl(var(--secondary))",
+        second: "hsl(var(--foreground-second))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        muted: {
+          foreground: "hsl(var(--muted-foreground))",
+        },
+      },
     },
   },
   plugins: [],
 };
+
 export default config;
