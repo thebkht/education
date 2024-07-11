aed81976d8dd0530f226ab24a3822d97b7400369
Bakhtiyor Ganijon <me@bkhtdev.com>
2024-07-11 16:32:39 +0500

    refactor: Move Button component to Button/index.tsx

---
+++ components/UI/Button/index.tsx
@@ -0,0 +1,40 @@
+import { cn } from "@/lib/uitls"
+import { cva, type VariantProps } from "class-variance-authority"
+
+const buttonVariants = cva(
+     "inline-flex items-center justify-center rounded-[4px] text-sm font-bold transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+     {
+          variants: {
+               variant: {
+                    default:
+                         "bg-primary border border-primary text-primary-foreground",
+                    outline:
+                         "border border-secondary bg-background hover:bg-secondary hover:text-primary-foreground",
+                    secondary: "bg-primary-foreground border border-primary-foreground text-primary",
+               },
+               size: {
+                    default: "px-4 py-3",
+                    sm: "px-4 py-2 text-xs",
+                    lg: "rounded-lg px-6 py-3 text-base font-medium",
+               },
+          },
+          defaultVariants: {
+               variant: "default",
+               size: "default",
+          },
+     }
+)
+
+export interface ButtonProps
+     extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+     VariantProps<typeof buttonVariants> {
+}
+
+export default function Index({ className, variant, size, ...props }: ButtonProps) {
+     return (
+          <button
+               className={cn(buttonVariants({ variant, size, className }))}
+               {...props}
+          />
+     )
+}
