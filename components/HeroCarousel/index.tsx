4d1e141cf74de368af8d5b21a3683794f2230714
Bakhtiyor Ganijon <me@bkhtdev.com>
2024-07-11 17:40:03 +0500

    refactor: Add Swiper component for image carousel in Hero section

---
+++ components/HeroCarousel/index.tsx
@@ -0,0 +1,191 @@
+import Image from "next/image";
+import Balancer from "react-wrap-balancer";
+
+import { Icons } from "@/components/icons";
+import Button from "@/components/UI/Button";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { Navigation, Pagination, A11y } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+export default function Index() {
+     return (
+          <>
+               {" "}
+               <Swiper
+                    modules={[Navigation, Pagination, A11y]}
+                    slidesPerView={1}
+                    navigation
+                    className="h-[428px]"
+                    loop={true}
+                    autoplay={{
+                         delay: 2500,
+                         disableOnInteraction: false,
+                    }}
+                    pagination={{
+                         clickable: true,
+                    }}
+                    scrollbar={{
+                         draggable: true,
+                    }}
+               >
+                    {" "}
+                    <SwiperSlide className="!flex items-stretch rounded-lg bg-primary">
+                         {" "}
+                         <div className="flex-1 relative flex flex-col justify-center">
+                              {" "}
+                              <div className="flex flex-col gap-4 w-[446px] text-primary-foreground mx-auto">
+                                   {" "}
+                                   <h1 className="text-4xl font-bold">
+                                        {" "}
+                                        <Balancer>
+                                             {" "}
+                                             O&apos; zbekistonga jadal islohotlarga mos malakali kadrlar
+                                             kerak{" "}
+                                        </Balancer>{" "}
+                                   </h1>{" "}
+                                   <p className="text-2xl font-medium text-right">
+                                        Sh.M Mirziyoyev
+                                   </p>{" "}
+                                   <Button variant={"secondary"} size={"lg"} className="w-fit">
+                                        {" "}
+                                        Kursni boshlash{" "}
+                                   </Button>{" "}
+                              </div>{" "}
+                              <div>
+                                   {" "}
+                                   <Icons.pattern className="w-full object-cover absolute top-0 left-0" />{" "}
+                              </div>{" "}
+                         </div>{" "}
+                         <div className="flex-1 relative">
+                              {" "}
+                              <div className="h-full w-full bg-primary absolute rounded-r-lg z-10 opacity-50"></div>{" "}
+                              <Image
+                                   src="/hero-img.png"
+                                   alt="Hero"
+                                   fill
+                                   className="rounded-r-lg !relative"
+                              />{" "}
+                         </div>{" "}
+                    </SwiperSlide>{" "}
+                    <SwiperSlide className="!flex items-stretch rounded-lg bg-primary">
+                         {" "}
+                         <div className="flex-1 relative flex flex-col justify-center">
+                              {" "}
+                              <div className="flex flex-col gap-4 w-[446px] text-primary-foreground mx-auto">
+                                   {" "}
+                                   <h1 className="text-4xl font-bold">
+                                        {" "}
+                                        <Balancer>
+                                             {" "}
+                                             O&apos; zbekistonga jadal islohotlarga mos malakali kadrlar
+                                             kerak{" "}
+                                        </Balancer>{" "}
+                                   </h1>{" "}
+                                   <p className="text-2xl font-medium text-right">
+                                        Sh.M Mirziyoyev
+                                   </p>{" "}
+                                   <Button variant={"secondary"} size={"lg"} className="w-fit">
+                                        {" "}
+                                        Kursni boshlash{" "}
+                                   </Button>{" "}
+                              </div>{" "}
+                              <div>
+                                   {" "}
+                                   <Icons.pattern className="w-full object-cover absolute top-0 left-0" />{" "}
+                              </div>{" "}
+                         </div>{" "}
+                         <div className="flex-1 relative">
+                              {" "}
+                              <div className="h-full w-full bg-primary absolute rounded-r-lg z-10 opacity-50"></div>{" "}
+                              <Image
+                                   src="/hero-img.png"
+                                   alt="Hero"
+                                   fill
+                                   className="rounded-r-lg !relative"
+                              />{" "}
+                         </div>{" "}
+                    </SwiperSlide>{" "}
+                    <SwiperSlide className="!flex items-stretch rounded-lg bg-primary">
+                         {" "}
+                         <div className="flex-1 relative flex flex-col justify-center">
+                              {" "}
+                              <div className="flex flex-col gap-4 w-[446px] text-primary-foreground mx-auto">
+                                   {" "}
+                                   <h1 className="text-4xl font-bold">
+                                        {" "}
+                                        <Balancer>
+                                             {" "}
+                                             O&apos; zbekistonga jadal islohotlarga mos malakali kadrlar
+                                             kerak{" "}
+                                        </Balancer>{" "}
+                                   </h1>{" "}
+                                   <p className="text-2xl font-medium text-right">
+                                        Sh.M Mirziyoyev
+                                   </p>{" "}
+                                   <Button variant={"secondary"} size={"lg"} className="w-fit">
+                                        {" "}
+                                        Kursni boshlash{" "}
+                                   </Button>{" "}
+                              </div>{" "}
+                              <div>
+                                   {" "}
+                                   <Icons.pattern className="w-full object-cover absolute top-0 left-0" />{" "}
+                              </div>{" "}
+                         </div>{" "}
+                         <div className="flex-1 relative">
+                              {" "}
+                              <div className="h-full w-full bg-primary absolute rounded-r-lg z-10 opacity-50"></div>{" "}
+                              <Image
+                                   src="/hero-img.png"
+                                   alt="Hero"
+                                   fill
+                                   className="rounded-r-lg !relative"
+                              />{" "}
+                         </div>{" "}
+                    </SwiperSlide>{" "}
+                    <SwiperSlide className="!flex items-stretch rounded-lg bg-primary">
+                         {" "}
+                         <div className="flex-1 relative flex flex-col justify-center">
+                              {" "}
+                              <div className="flex flex-col gap-4 w-[446px] text-primary-foreground mx-auto">
+                                   {" "}
+                                   <h1 className="text-4xl font-bold">
+                                        {" "}
+                                        <Balancer>
+                                             {" "}
+                                             O&apos; zbekistonga jadal islohotlarga mos malakali kadrlar
+                                             kerak{" "}
+                                        </Balancer>{" "}
+                                   </h1>{" "}
+                                   <p className="text-2xl font-medium text-right">
+                                        Sh.M Mirziyoyev
+                                   </p>{" "}
+                                   <Button variant={"secondary"} size={"lg"} className="w-fit">
+                                        {" "}
+                                        Kursni boshlash{" "}
+                                   </Button>{" "}
+                              </div>{" "}
+                              <div>
+                                   {" "}
+                                   <Icons.pattern className="w-full object-cover absolute top-0 left-0" />{" "}
+                              </div>{" "}
+                         </div>{" "}
+                         <div className="flex-1 relative">
+                              {" "}
+                              <div className="h-full w-full bg-primary absolute rounded-r-lg z-10 opacity-50"></div>{" "}
+                              <Image
+                                   src="/hero-img.png"
+                                   alt="Hero"
+                                   fill
+                                   className="rounded-r-lg !relative"
+                              />{" "}
+                         </div>{" "}
+                    </SwiperSlide>{" "}
+               </Swiper>{" "}
+          </>
+     );
+}
--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -1,32 +1,10 @@
-import Image from "next/image";
-import Balancer from "react-wrap-balancer";
-import { Icons } from "@/components/icons";
-import Button from "@/components/UI/Button";
+import HeroCarousel from '@/components/HeroCarousel'
 
 export function Hero() {
      return (
           <>
-               <div className="container flex items-stretch rounded-lg mx-auto bg-primary">
-                    <div className="flex-1 relative flex flex-col justify-center">
-                         <div className="flex flex-col gap-4 w-[446px] text-primary-foreground mx-auto">
-                              <h1 className="text-4xl font-bold">
-                                   <Balancer>
-                                        O&apos;zbekistonga jadal islohotlarga mos malakali kadrlar kerak
-                                   </Balancer>
-                              </h1>
-                              <p className="text-2xl font-medium text-right">Sh.M Mirziyoyev</p>
-                              <Button variant={'secondary'} size={'lg'} className="w-fit">
-                                   Kursni boshlash
-                              </Button>
-                         </div>
-                         <div>
-                              <Icons.pattern className="w-full object-cover absolute top-0 left-0" />
-                         </div>
-                    </div>
-                    <div className="flex-1 relative">
-                         <div className="h-full w-full bg-primary absolute rounded-r-lg z-10 opacity-50"></div>
-                         <Image src="/hero-img.png" alt="Hero" fill className="rounded-r-lg !relative" />
-                    </div>
+               <div className="container mx-auto mb-8">
+                    <HeroCarousel />
                </div>
           </>
      )
