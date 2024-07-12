060ffa40e38fed1fe6db0fe5cfed4052d47f751c
Bakhtiyor Ganijon <me@bkhtdev.com>
2024-07-12 11:45:22 +0500

    refactor: Improve HeroCarousel component layout and responsiveness

--- components/HeroCarousel/index.tsx
+++ components/HeroCarousel/index.tsx
@@ -14,13 +14,16 @@ import "swiper/css/pagination";
 export default function Index() {
      return (
           <>
-               {" "}
+
                <Swiper
-                    modules={[Navigation, Pagination, A11y]}
+                    modules={[Navigation, Pagination, A11y, Autoplay]}
                     navigation
                     className="h-[428px]"
                     loop={true}
-
+                    autoplay={{
+                         delay: 5000,
+                         disableOnInteraction: false,
+                    }}
                     pagination={{
                          clickable: true,
                     }}
@@ -28,156 +31,160 @@ export default function Index() {
                          draggable: true,
                     }}
                >
-                    {" "}
+
                     <SwiperSlide className="!flex items-stretch rounded-lg bg-primary">
                          <div className="flex-1 relative flex flex-col justify-center">
-                              {" "}
+
                               <div className="flex flex-col gap-4 lg:w-[446px] p-6 lg:p-0 text-primary-foreground mx-auto">
-                                   {" "}
+
                                    <h1 className="lg:text-4xl text-3xl font-bold">
-                                        {" "}
+
                                         <Balancer>
-                                             {" "}
+
                                              O&apos;zbekistonga jadal islohotlarga mos malakali kadrlar
-                                             kerak{" "}
-                                        </Balancer>{" "}
-                                   </h1>{" "}
+                                             kerak
+                                        </Balancer>
+                                   </h1>
                                    <p className="lg:text-2xl text-xl font-medium text-right">
                                         Sh.M Mirziyoyev
-                                   </p>{" "}
-                                   <Button variant={"secondary"} size={"lg"} className="w-fit">
-                                        {" "}
-                                        Kursni boshlash{" "}
-                                   </Button>{" "}
-                              </div>{" "}
+                                   </p>
+                                   <Button variant={"secondary"} size={"lg"} className="w-fit"
+                                   >
+
+                                        Kursni boshlash
+                                   </Button>
+                              </div>
                               <div>
-                                   {" "}
-                                   <Icons.pattern className="w-full object-cover absolute top-0 left-0" />{" "}
-                              </div>{" "}
-                         </div>{" "}
+
+                                   <Icons.pattern className="w-full h-full object-cover absolute pointer-events-none top-0 left-0" />
+                              </div>
+                         </div>
                          <div className="flex-1 relative md:block hidden">
-                              {" "}
-                              <div className="h-full w-full bg-primary absolute rounded-r-lg z-10 opacity-50"></div>{" "}
+
+                              <div className="h-full w-full bg-primary absolute rounded-r-lg z-10 opacity-50"></div>
                               <Image
                                    src="/hero-img.png"
                                    alt="Hero"
                                    fill
                                    className="rounded-r-lg !relative"
-                              />{" "}
+                              />
                          </div>
-                    </SwiperSlide>{" "}
+                    </SwiperSlide>
                     <SwiperSlide className="!flex items-stretch rounded-lg bg-primary">
                          <div className="flex-1 relative flex flex-col justify-center">
-                              {" "}
+
                               <div className="flex flex-col gap-4 lg:w-[446px] p-6 lg:p-0 text-primary-foreground mx-auto">
-                                   {" "}
+
                                    <h1 className="lg:text-4xl text-3xl font-bold">
-                                        {" "}
+
                                         <Balancer>
-                                             {" "}
+
                                              O&apos;zbekistonga jadal islohotlarga mos malakali kadrlar
-                                             kerak{" "}
-                                        </Balancer>{" "}
-                                   </h1>{" "}
+                                             kerak
+                                        </Balancer>
+                                   </h1>
                                    <p className="lg:text-2xl text-xl font-medium text-right">
                                         Sh.M Mirziyoyev
-                                   </p>{" "}
-                                   <Button variant={"secondary"} size={"lg"} className="w-fit">
-                                        {" "}
-                                        Kursni boshlash{" "}
-                                   </Button>{" "}
-                              </div>{" "}
+                                   </p>
+                                   <Button variant={"secondary"} size={"lg"} className="w-fit"
+                                   >
+
+                                        Kursni boshlash
+                                   </Button>
+                              </div>
                               <div>
-                                   {" "}
-                                   <Icons.pattern className="w-full object-cover absolute top-0 left-0 z-0" />{" "}
-                              </div>{" "}
-                         </div>{" "}
+
+                                   <Icons.pattern className="w-full h-full object-cover absolute pointer-events-none top-0 left-0" />
+                              </div>
+                         </div>
                          <div className="flex-1 relative md:block hidden">
-                              {" "}
-                              <div className="h-full w-full bg-primary absolute rounded-r-lg z-10 opacity-50"></div>{" "}
+
+                              <div className="h-full w-full bg-primary absolute rounded-r-lg z-10 opacity-50"></div>
                               <Image
                                    src="/hero-img.png"
                                    alt="Hero"
                                    fill
                                    className="rounded-r-lg !relative"
-                              />{" "}
+                              />
                          </div>
-                    </SwiperSlide>{" "}
+                    </SwiperSlide>
                     <SwiperSlide className="!flex items-stretch rounded-lg bg-primary">
                          <div className="flex-1 relative flex flex-col justify-center">
-                              {" "}
+
                               <div className="flex flex-col gap-4 lg:w-[446px] p-6 lg:p-0 text-primary-foreground mx-auto">
-                                   {" "}
+
                                    <h1 className="lg:text-4xl text-3xl font-bold">
-                                        {" "}
+
                                         <Balancer>
-                                             {" "}
+
                                              O&apos;zbekistonga jadal islohotlarga mos malakali kadrlar
-                                             kerak{" "}
-                                        </Balancer>{" "}
-                                   </h1>{" "}
+                                             kerak
+                                        </Balancer>
+                                   </h1>
                                    <p className="lg:text-2xl text-xl font-medium text-right">
                                         Sh.M Mirziyoyev
-                                   </p>{" "}
-                                   <Button variant={"secondary"} size={"lg"} className="w-fit">
-                                        {" "}
-                                        Kursni boshlash{" "}
-                                   </Button>{" "}
-                              </div>{" "}
+                                   </p>
+                                   <Button variant={"secondary"} size={"lg"} className="w-fit"
+                                   >
+
+                                        Kursni boshlash
+                                   </Button>
+                              </div>
                               <div>
-                                   {" "}
-                                   <Icons.pattern className="w-full object-cover absolute top-0 left-0" />{" "}
-                              </div>{" "}
-                         </div>{" "}
+
+                                   <Icons.pattern className="w-full h-full object-cover absolute pointer-events-none top-0 left-0" />
+                              </div>
+                         </div>
                          <div className="flex-1 relative md:block hidden">
-                              {" "}
-                              <div className="h-full w-full bg-primary absolute rounded-r-lg z-10 opacity-50"></div>{" "}
+
+                              <div className="h-full w-full bg-primary absolute rounded-r-lg z-10 opacity-50"></div>
                               <Image
                                    src="/hero-img.png"
                                    alt="Hero"
                                    fill
                                    className="rounded-r-lg !relative"
-                              />{" "}
+                              />
                          </div>
-                    </SwiperSlide>{" "}
+                    </SwiperSlide>
                     <SwiperSlide className="!flex items-stretch rounded-lg bg-primary">
                          <div className="flex-1 relative flex flex-col justify-center">
-                              {" "}
+
                               <div className="flex flex-col gap-4 lg:w-[446px] p-6 lg:p-0 text-primary-foreground mx-auto">
-                                   {" "}
+
                                    <h1 className="lg:text-4xl text-3xl font-bold">
-                                        {" "}
+
                                         <Balancer>
-                                             {" "}
+
                                              O&apos;zbekistonga jadal islohotlarga mos malakali kadrlar
-                                             kerak{" "}
-                                        </Balancer>{" "}
-                                   </h1>{" "}
+                                             kerak
+                                        </Balancer>
+                                   </h1>
                                    <p className="lg:text-2xl text-xl font-medium text-right">
                                         Sh.M Mirziyoyev
-                                   </p>{" "}
-                                   <Button variant={"secondary"} size={"lg"} className="w-fit">
-                                        {" "}
-                                        Kursni boshlash{" "}
-                                   </Button>{" "}
-                              </div>{" "}
+                                   </p>
+                                   <Button variant={"secondary"} size={"lg"} className="w-fit"
+                                   >
+
+                                        Kursni boshlash
+                                   </Button>
+                              </div>
                               <div>
-                                   {" "}
-                                   <Icons.pattern className="w-full object-cover absolute top-0 left-0" />{" "}
-                              </div>{" "}
-                         </div>{" "}
+
+                                   <Icons.pattern className="w-full h-full object-cover absolute pointer-events-none top-0 left-0" />
+                              </div>
+                         </div>
                          <div className="flex-1 relative md:block hidden">
-                              {" "}
-                              <div className="h-full w-full bg-primary absolute rounded-r-lg z-10 opacity-50"></div>{" "}
+
+                              <div className="h-full w-full bg-primary absolute rounded-r-lg z-10 opacity-50"></div>
                               <Image
                                    src="/hero-img.png"
                                    alt="Hero"
                                    fill
                                    className="rounded-r-lg !relative"
-                              />{" "}
+                              />
                          </div>
-                    </SwiperSlide>{" "}
-               </Swiper>{" "}
+                    </SwiperSlide>
+               </Swiper>
           </>
      );
 }
