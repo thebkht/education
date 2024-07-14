4644fafc3848812ce31133f614a75fe2862a19f8
Bakhtiyor Ganijon <me@bkhtdev.com>
2024-07-14 22:11:05 +0500

    refactor: Update Input and Label components for improved accessibility and styling consistency

---
+++ components/UI/Label/index.tsx
@@ -0,0 +1,22 @@
+// Corrected TypeScript code for the React component
+import * as React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "@/lib/uitls"
+import * as LabelPrimitive from "@radix-ui/react-label"
+
+const labelVariants = cva("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70");
+
+type IndexProps = React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> & {
+     className?: string;
+} & VariantProps<typeof labelVariants>;
+
+const Index: React.FC<IndexProps> = ({ className, ...props }) => {
+     return (
+          <LabelPrimitive.Root
+               className={cn(labelVariants(), className)}
+               {...props}
+          />
+     );
+};
+
+export default Index;
