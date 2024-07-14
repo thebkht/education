1fdbf17dff4f083018f72fdb6fd2beeeab06e802
Bakhtiyor Ganijon <me@bkhtdev.com>
2024-07-14 22:13:30 +0500

    refactor: Update Input component to use custom Input component for improved accessibility and styling consistency

--- pages/courses/index.tsx
+++ pages/courses/index.tsx
@@ -2,6 +2,7 @@ import CourseLayout from '@/components/layout/CourseLayout';
 import { courses } from '@/data/courses';
 import CourseCard from '@/components/CourseCard2';
 import { Icons } from '@/components/icons';
+import Input from '@/components/UI/Input';
 
 export default function Courses() {
      return (
@@ -9,7 +10,7 @@ export default function Courses() {
                <CourseLayout>
                     <div className="flex gap-6 items-center">
                          <div className="bg-background w-60 rounded-[4px] py-2.5 px-4 flex items-center gap-2">
-                              <input type="text" placeholder="Поиск сотрудника" className="bg-transparent border-none focus:outline-none w-full" />
+                              <Input type="text" placeholder="Поиск сотрудника" className="bg-transparent border-none focus:outline-none w-full focus-visible:ring-offset-0 focus-visible:ring-0" />
                               <Icons.search className="w-4 h-4 text-muted-foreground" />
                          </div>
                     </div>
