b5041c69f2dfafb81b15e86daa80990d008137e5
Bakhtiyor Ganijon <me@bkhtdev.com>
2024-07-15 11:48:36 +0500

    refactor: Improve CourseCard2 component layout and use aspect ratio for video

--- components/CourseCard2/index.tsx
+++ components/CourseCard2/index.tsx
@@ -7,21 +7,21 @@ export default function Index({ course }: { course: any }) {
           <div className="flex flex-col gap-2 rounded-lg border border-border bg-background p-4">
                <div className="flex justify-end items-end flex-1 aspect-video relative">
                     {
-                         course.new && (
-                              <div className="flex gap-2 py-2 px-3 border-t border-l border-border rounded-tl-[4px] bg-background text-second text-sm absolute right-0 bottom-0 z-20 items-center justify-center">
-                                   <div className="h-3 w-3 bg-blue-600 rounded-full"></div>
-                                   <span>Новый</span>
-                              </div>
-                         )
-                    }
-                    {
                          course.locked ? (
-                              <div className="h-full w-full bg-foreground/40 flex justify-center items-center rounded-[4px] absolute z-10 backdrop-blur-lg">
+                              <div className="h-full w-full bg-black/40 flex justify-center items-center rounded absolute z-10 backdrop-blur-lg">
                                    <Icons.lock className="text-background h-12 w-12" />
                               </div>
                          ) : (
-                              <div className="h-full w-full bg-foreground/40 rounded-[4px] absolute z-10"></div>
+                              <div className="h-full w-full bg-black/40 rounded absolute z-10"></div>
                          )
+                    }
+                    {
+                        course.new && (
+                            <div className="flex gap-2 py-2 px-3 border-t border-l border-border rounded-tl bg-background text-second text-sm absolute right-0 bottom-0 z-20 items-center justify-center">
+                                 <div className="h-3 w-3 bg-blue-600 rounded-full"></div>
+                                 <span>Новый</span>
+                            </div>
+                        )
                     }
                     <Image src={course.image} alt={course.title} layout="fill" className="rounded-[4px] object-cover !relative border border-border aspect-video" />
                </div>
