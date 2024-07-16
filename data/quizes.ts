1f8688284feac59d0e4d124fe913c0f3c93780fa
Bakhtiyor Ganijon <me@bkhtdev.com>
2024-07-16 05:30:47 +0500

    refactor: Add quizes fake data file

---
+++ data/quizes.ts
@@ -0,0 +1,115 @@
+export const quizes = [
+  {
+    id: 1,
+    question:
+      "Forma A-1. Ta’mirlanadigan avtomobil yo‘li yoki ko‘prik, quvur, avtobekat ishlarini qabul qilish dalolatnomasi.",
+    options: [
+      {
+        id: 1,
+        value: "Bajarilgan ishlar vedomosti;",
+      },
+      {
+        id: 2,
+        value: "Kamchiliklar va uni tugatish vedomosti;",
+      },
+      {
+        id: 3,
+        value: "Nuqsonlar qaydnomasi va tuzatish muddati;",
+      },
+      {
+        id: 4,
+        value: "Nazorat o‘lchovlari va tadqiqotlar vedomosti;",
+      },
+      {
+        id: 5,
+        value: "Hujjatlar ro‘yxati.",
+      },
+    ],
+    correctAnswer: 3,
+  },
+  {
+    id: 2,
+    question:
+      "Какие из следующих типов данных являются примитивными в JavaScript?",
+    options: [
+      {
+        id: 1,
+        value: "String",
+      },
+      {
+        id: 2,
+        value: "Number",
+      },
+      {
+        id: 3,
+        value: "Оба из них",
+      },
+    ],
+    correctAnswer: 3,
+  },
+  {
+    id: 3,
+    question: "Что такое Null в JavaScript?",
+    options: [
+      {
+        id: 1,
+        value: "Null означает пустое строковое значение.",
+      },
+      {
+        id: 2,
+        value: "Null означает отсутствие значения.",
+      },
+      {
+        id: 3,
+        value: "Null означает неизвестное значение.",
+      },
+    ],
+    correctAnswer: 2,
+  },
+  {
+    id: 4,
+    question: "Что такое замыкание в JavaScript?",
+    options: [
+      {
+        id: 1,
+        value: "Функция, возвращающая функцию",
+      },
+      {
+        id: 2,
+        value: "Функция, возвращающая объект",
+      },
+      {
+        id: 3,
+        value: "Объект, возвращающий функцию",
+      },
+      {
+        id: 4,
+        value: "Объект, возвращающий объект",
+      },
+    ],
+    correctAnswer: 1,
+  },
+  {
+    id: 5,
+    question: "Что такое замыкание в JavaScript?",
+    options: [
+      {
+        id: 1,
+        value: "Функция, возвращающая функцию",
+      },
+      {
+        id: 2,
+        value: "Функция, возвращающая объект",
+      },
+      {
+        id: 3,
+        value: "Объект, возвращающий функцию",
+      },
+      {
+        id: 4,
+        value: "Объект, возвращающий объект",
+      },
+    ],
+    correctAnswer: 1,
+  },
+];
