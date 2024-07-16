22b3cacdc296c98ab25cce469d15e8d2c81d520e
Bakhtiyor Ganijon <me@bkhtdev.com>
2024-07-16 09:15:18 +0500

    refactor: Update CourseCard2

--- components/CourseCard2/index.tsx
+++ components/CourseCard2/index.tsx
@@ -9,16 +9,33 @@ import {
      AlertDialogTrigger
 } from "@/components/UI/AlertDialog";
 import Balancer from "react-wrap-balancer";
-import {Dialog, DialogContent, DialogTrigger} from "@/components/UI/DIalog";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/UI/DIalog";
 import Link from "next/link";
+import React from "react";
 
 export default function Index({ course }: { course: any }) {
+     const [open, setOpen] = React.useState(false);
      return (<>
+          <AlertDialog open={open} onOpenChange={setOpen}>
+               <AlertDialogContent>
+                    <AlertDialogDescription className={'text-center'}>
+                         <Balancer>
+                              Если вы произвели оплату, но курсы не открылись, обратитесь к администратору системы.
+                              тел: +998 (71) 123-45-67
+                         </Balancer>
+                    </AlertDialogDescription>
+                    <AlertDialogAction className={'w-fit mx-auto'}>
+                         Понятно
+                    </AlertDialogAction>
+               </AlertDialogContent>
+          </AlertDialog>
           <div className="flex flex-col gap-2 rounded-lg border border-border bg-background p-4">
                <div className="flex justify-end items-end flex-1 aspect-video relative">
                     {
                          course.locked ? (
-                              <div className="h-full w-full bg-black/40 flex justify-center items-center rounded absolute z-10 backdrop-blur-lg pointer-events-none">
+                              <div className="h-full w-full bg-black/40 flex justify-center items-center rounded absolute z-10 backdrop-blur-lg cursor-pointer" onClick={
+                                   () => setOpen(true)
+                              }>
                                    <Icons.lock className="text-background h-12 w-12" />
                               </div>
                          ) : (
@@ -26,14 +43,21 @@ export default function Index({ course }: { course: any }) {
                          )
                     }
                     {
-                        course.new && (
-                            <div className="flex gap-2 py-2 px-3 border-t border-l border-border rounded-tl bg-background text-second text-sm absolute right-0 bottom-0 z-20 items-center justify-center">
-                                 <div className="h-3 w-3 bg-blue-600 rounded-full"></div>
-                                 <span>Новый</span>
-                            </div>
-                        )
+                         course.new && (
+                              <div className="flex gap-2 py-2 px-3 border-t border-l border-border rounded-tl bg-background text-second text-sm absolute right-0 bottom-0 z-20 items-center justify-center">
+                                   <div className="h-3 w-3 bg-blue-600 rounded-full"></div>
+                                   <span>Новый</span>
+                              </div>
+                         )
                     }
-                    <Link href={course.locked ? "#" : `/courses/${course.slug}`} className={course.locked && "pointer-events-none"}>
+                    <Link href={course.locked ? "#" : `/courses/${course.slug}`}
+                         onClick={
+                              () => {
+                                   if (course.locked) {
+                                        setOpen(true)
+                                   }
+                              }
+                         }>
                          <Image src={course.image} alt={course.title} layout="fill" className="rounded-[4px] object-cover !relative border border-border aspect-video" />
                     </Link>
                </div>
@@ -47,45 +71,43 @@ export default function Index({ course }: { course: any }) {
                                    <span className="text-xs text-muted-foreground">{course.students || 0}</span>
                               </div>
                          </div>
-                         <Link href={course.locked ? "#" : `/courses/${course.slug}`} className={course.locked && "pointer-events-none"}>
+                         <Link href={course.locked ? "#" : `/courses/${course.slug}`}
+                              onClick={
+                                   () => {
+                                        if (course.locked) {
+                                             setOpen(true)
+                                        }
+                                   }
+                              }>
                               <h1 className="font-semibold text-second text-sm line-clamp-2">
                                    {course.title}
                               </h1>
                          </Link>
                     </div>
-                    <Link href={course.locked ? "#" : `/courses/${course.slug}`} className={course.locked && "pointer-events-none"}>
+                    <Link href={course.locked ? "#" : `/courses/${course.slug}`}
+                         onClick={
+                              () => {
+                                   if (course.locked) {
+                                        setOpen(true)
+                                   }
+                              }
+                         }>
                          <p className="text-xs text-muted-foreground line-clamp-3">
                               {course.description}
                          </p>
                     </Link>
                     {
-                         course.locked ? (
-                             // <AlertDialog>
-                             //      <AlertDialogTrigger asChild>
-                             //           <Button>Заключить договор на курс</Button>
-                             //      </AlertDialogTrigger>
-                             //      <AlertDialogContent>
-                             //           <AlertDialogDescription className={'text-center'}>
-                             //                <Balancer>
-                             //                     Если вы произвели оплату, но курсы не открылись, обратитесь к администратору системы.
-                             //                     тел: +998 (71) 123-45-67
-                             //                </Balancer>
-                             //           </AlertDialogDescription>
-                             //           <AlertDialogAction className={'w-fit mx-auto'}>
-                             //                Понятно
-                             //           </AlertDialogAction>
-                             //      </AlertDialogContent>
-                             // </AlertDialog>
-                             <Dialog>
-                                  <DialogTrigger asChild>
-                                       <Button className={"w-fit rounded"}>Заключить договор на курс</Button>
-                                  </DialogTrigger>
-                                  <DialogContent className={"p-0 max-w-[984px] max-h-[948px] h-full border-none mb-3"} noClose>
-                                       <object data="" type="application/pdf" className={"h-full w-full"}></object>
-                                  </DialogContent>
-                             </Dialog>
-                         ): (
-                             <Button className={"w-fit rounded"} disabled>Заключить договор на курс</Button>
+                         !course.locked ? (
+                              <Dialog>
+                                   <DialogTrigger asChild>
+                                        <Button className={"w-fit rounded"}>Заключить договор на курс</Button>
+                                   </DialogTrigger>
+                                   <DialogContent className={"p-0 max-w-[984px] max-h-[948px] h-full border-none mb-3"} noClose>
+                                        <object data="" type="application/pdf" className={"h-full w-full"}></object>
+                                   </DialogContent>
+                              </Dialog>
+                         ) : (
+                              <Button className={"w-fit rounded"} disabled>Заключить договор на курс</Button>
                          )
                     }
                </div>
