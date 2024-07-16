28b86f9c2266cb1f7fcd286155886a1f0e81bd3b
Bakhtiyor Ganijon <me@bkhtdev.com>
2024-07-16 09:26:49 +0500

    refactor: Update course chapters data structure

--- data/course-chapters.ts
+++ data/course-chapters.ts
@@ -1,53 +1,61 @@
-export const chapters = [
-    {
-        title: 'Введение',
-        videos: 14,
-        quizes: 4,
-        desc: `O‘quv dasturi “Avtomobil yo‘llari ilmiy-tadqiqot instituti” unitar korxonasining ilmiy-texnik kengashida muxokama qilinib,
+export const sections = [
+  {
+    id: 1,
+    title: "Введение",
+    videos: 14,
+    quizes: 4,
+    desc: `O‘quv dasturi “Avtomobil yo‘llari ilmiy-tadqiqot instituti” unitar korxonasining ilmiy-texnik kengashida muxokama qilinib,
 2023-yil __ - sonli bayonnoma bilan maqullangan.`,
-        sections: [
-            {
-                title: `Yo‘l qurilish, ta’mirlash ishlarini tashkil qilish, (PPR-ishni amalga oshirish rejasi) xarajatlarni aniqlash va hujjatlarni rasmiylashtirish qoidasi`,
-                completed: false,
-                locked: false,
-                hasQuiz: true
-            },
-            {
-                title: `Bajariladigan ishlar davomiyligini ish turlari bo‘yicha hisoblash va ishlarni  bajarish kalendar rejasini tuzish`,
-                locked: true,
-                completed: false,
-                hasQuiz: false,
-            },
-            {
-                title: `Asos va qoplamalarni qurish ishlari  texnologiyalari, ular orasidagi farqlar. Ish bajarishdagi texnik va operatsion nazorat kriteriyalarih`,
-                locked: true,
-                completed: false,
-                hasQuiz: true,
-            },
-            {
-                title: `Yo‘l qurilish materiallari xossalari va qo‘llanishi. Maxalliy materiallar.`,
-                locked: true,
-                completed: false,
-                hasQuiz: true,
-            },
-            {
-                title: `Yo‘l qurilishida Geodeziya. Teodolit, nivelir va turli xil o‘lchov asboblari bilan geodezik nuqtalarni aniqlashni o‘rgatish. JPS –avtomatik aniqlash-hisoblash usullari.`,
-                locked: true,
-                completed: false,
-                hasQuiz: false,
-            },
-            {
-                title: `Laboratoriya ishlarida sifat nazorati`,
-                locked: true,
-                completed: false,
-                hasQuiz: false,
-            },
-            {
-                title: `Yakuniy nazorat, muloqot va test sinovlari`,
-                locked: true,
-                completed: false,
-                hasQuiz: true,
-            },
-        ]
-    },
-]
+    lectures: [
+      {
+        id: 1,
+        title: `Yo‘l qurilish, ta’mirlash ishlarini tashkil qilish, (PPR-ishni amalga oshirish rejasi) xarajatlarni aniqlash va hujjatlarni rasmiylashtirish qoidasi`,
+        completed: false,
+        locked: false,
+        hasQuiz: true,
+      },
+      {
+        id: 2,
+        title: `Bajariladigan ishlar davomiyligini ish turlari bo‘yicha hisoblash va ishlarni  bajarish kalendar rejasini tuzish`,
+        locked: true,
+        completed: false,
+        hasQuiz: false,
+      },
+      {
+        id: 3,
+        title: `Asos va qoplamalarni qurish ishlari  texnologiyalari, ular orasidagi farqlar. Ish bajarishdagi texnik va operatsion nazorat kriteriyalarih`,
+        locked: true,
+        completed: false,
+        hasQuiz: true,
+      },
+      {
+        id: 4,
+        title: `Yo‘l qurilish materiallari xossalari va qo‘llanishi. Maxalliy materiallar.`,
+        locked: true,
+        completed: false,
+        hasQuiz: true,
+      },
+      {
+        id: 5,
+        title: `Yo‘l qurilishida Geodeziya. Teodolit, nivelir va turli xil o‘lchov asboblari bilan geodezik nuqtalarni aniqlashni o‘rgatish. JPS –avtomatik aniqlash-hisoblash usullari.`,
+        locked: true,
+        completed: false,
+        hasQuiz: false,
+      },
+      {
+        id: 6,
+        title: `Laboratoriya ishlarida sifat nazorati`,
+        locked: true,
+        completed: false,
+        hasQuiz: false,
+      },
+      {
+        id: 7,
+        title: `Yakuniy nazorat, muloqot va test sinovlari`,
+        locked: true,
+        completed: false,
+        hasQuiz: true,
+      },
+    ],
+  },
+];
