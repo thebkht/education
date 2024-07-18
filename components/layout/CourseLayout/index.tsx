7e1a6b04aca2f03811b11ae2296214adc8fbd97a
Bakhtiyor Ganijon <me@bkhtdev.com>
2024-07-18 14:22:47 +0500

    refactor: remove unused classes

--- components/layout/CourseLayout/index.tsx
+++ components/layout/CourseLayout/index.tsx
@@ -14,19 +14,19 @@ export default function Index({ children, className }: { children: React.ReactNo
                <div className={cn(styles.main, collapsed && styles.collapsed)}>
                     <div className="flex z-50">
                          <div className={`flex-[1_1_1px] ${collapsed ? "w-[72px]" : "w-[250px]"} h-14 px-4 justify-center flex flex-col fixed z-50 border-r border-border box-border`}>
-                              <Link href="/" className={"cursor-pointer relative flex select-none border-none"}>
+                              <Link href="/" className={"cursor-pointer relative flex select-none border-none rounded-lg"}>
                                    <Image src={collapsed ? "/favicon.ico" : "/logo.png"} alt="Logo" width={collapsed ? 45 : 141} height={45} className='transition-all duration-300 ease-in-out' />
                               </Link>
                          </div>
                     </div>
                     <Header collapsed={collapsed} onCollapse={setCollapsed} />
                     <Sidebar collapsed={collapsed} />
-                    <main className="flex relative justify-center z-0 min-h-0 min-w-0 flex-[1_0_auto] md:flex-none bg-accent">
+                    <main className="flex relative justify-center z-0 min-h-0 flex-[1_0_auto] bg-accent">
                          <div className="flex w-full flex-[1_0_auto]">
-                              <div className="flex flex-1 min-w-0">
-                                   <div className="flex justify-center flex-[1_0_1px] min-w-0 [overflow-anchor:none] overflow-x-auto">
-                                        <div className="flex flex-1 min-w-0">
-                                             <div className={cn("flex flex-1 justify-center min-w-0", className)}>
+                              <div className="flex flex-1">
+                                   <div className="flex justify-center flex-[1_0_1px] overflow-x-auto">
+                                        <div className="flex flex-1">
+                                             <div className={cn("flex flex-1 justify-center", className)}>
                                                   <div className="flex flex-col py-4 px-6 w-full">
                                                        {children}
                                                   </div>
