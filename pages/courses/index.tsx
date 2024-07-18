af71b8812a6f002ed92d27bd38db1e305a90ea64
Bakhtiyor Ganijon <me@bkhtdev.com>
2024-07-18 14:23:03 +0500

    refactor: Update Courses page layout and search functionality

--- pages/courses/index.tsx
+++ pages/courses/index.tsx
@@ -10,18 +10,20 @@ export default function Courses() {
           <>
                <Metadata title="Mening Kurslarim" description="Kurslar" />
                <CourseLayout>
-                    <div className="flex gap-6 items-center">
-                         <div className="bg-background w-60 rounded-[4px] py-2.5 px-4 flex items-center gap-2">
-                              <Input type="text" placeholder="Qidiruv" className="bg-transparent border-none focus:outline-none w-full focus-visible:ring-offset-0 focus-visible:ring-0 p-0 h-fit" />
-                              <Icons.search className="w-4 h-4 text-muted-foreground" />
+                    <div className="box-border">
+                         <div className="flex gap-6 items-center">
+                              <div className="bg-background w-60 rounded-[4px] py-2.5 px-4 flex items-center gap-2">
+                                   <Input type="text" placeholder="Qidiruv" className="bg-transparent border-none focus:outline-none w-full focus-visible:ring-offset-0 focus-visible:ring-0 p-0 h-fit" />
+                                   <Icons.search className="w-4 h-4 text-muted-foreground" />
+                              </div>
                          </div>
-                    </div>
-                    <div className="flex-1 grid xl:grid-cols-4 grid-cols-3 gap-x-6 gap-y-4 my-4">
-                         {
-                              courses.map((course, index) => (
-                                   <CourseCard key={index} course={course} />
-                              ))
-                         }
+                         <div className="flex-1 grid xl:grid-cols-4 grid-cols-3 gap-x-6 gap-y-4 my-4">
+                              {
+                                   courses.map((course, index) => (
+                                        <CourseCard key={index} course={course} />
+                                   ))
+                              }
+                         </div>
                     </div>
                </CourseLayout>
           </>
