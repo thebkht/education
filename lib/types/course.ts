a0d8cad5187ed917fbeb989c4168c1c52f9fa16b
Bakhtiyor Ganijon <me@bkhtdev.com>
2024-07-19 10:41:11 +0500

    refactor: Update Course type definition and related components

--- lib/types/course.ts
+++ lib/types/course.ts
@@ -9,3 +9,14 @@ const courseDetail = z.object({
 });
 
 export type CourseDetail = z.infer<typeof courseDetail>;
+
+const course = z.object({
+  id: z.string(),
+  name: z.string(),
+  short_description: z.string(),
+  has_access: z.boolean(),
+  image: z.string(),
+  contract_file: z.string().optional(),
+});
+
+export type Course = z.infer<typeof course>;
