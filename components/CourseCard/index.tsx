2e54930a93f7306ca90a25a489bdf02be2c754df
Bakhtiyor Ganijon <me@bkhtdev.com>
2024-07-19 10:34:49 +0500

    refactor: Update CourseCard component to use CourseDetail type and fix link URLs

--- components/CourseCard/index.tsx
+++ components/CourseCard/index.tsx
@@ -1,24 +1,25 @@
+import { CourseDetail } from "@/lib/types/course";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Index({ course, ...props }: { course: any } & React.ComponentPropsWithoutRef<"div">) {
+export default function Index({ course, ...props }: { course: CourseDetail } & React.ComponentPropsWithoutRef<"div">) {
      return (
           <>
-               <div className="flex flex-col gap-2 rounded-3xl">
-                    <Link href={`#/courses/${course.slug}`}>
+               <div className="flex flex-col gap-2 rounded-3xl" {...props}>
+                    <Link href={`#/courses/${course.id}`}>
                          <div className="h-40 rounded-lg w-full">
-                              <Image src={course.image} alt={course.title} layout="fill" className="rounded-lg object-cover !relative" />
+                              <Image src={course.image} alt={course.name} layout="fill" className="rounded-lg object-cover !relative" />
                          </div>
                     </Link>
                     <div className="flex flex-col gap-1">
-                         <Link href={`#/courses/${course.slug}`}>
+                         <Link href={`#/courses/${course.id}`}>
                               <h1 className="font-semibold text-second line-clamp-2">
-                                   {course.title}
+                                   {course.name}
                               </h1>
                          </Link>
                          <Link href={`#`}>
                               <p className="text-sm text-muted-foreground line-clamp-2">
-                                   {course.description}
+                                   {course.short_description}
                               </p>
                          </Link>
                     </div>
