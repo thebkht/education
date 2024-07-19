9b1834deb8f1b781bcdb3a17ff3d30be4c4a706f
Bakhtiyor Ganijon <me@bkhtdev.com>
2024-07-19 10:41:18 +0500

    refactor: Update fetchers to include getCourses and getCourse functions

--- lib/fetchers.ts
+++ lib/fetchers.ts
@@ -1,5 +1,19 @@
+import { CourseDetail } from "./types/course";
+
 export async function getAllCourses() {
   const res = await fetch(`${process.env.API_URL}/courses/all`);
+  const data = await res.json();
+  return data;
+}
+
+export async function getCourses() {
+  const res = await fetch(`${process.env.API_URL}/courses`);
+  const data = await res.json();
+  return data;
+}
+
+export async function getCourse({ id }: CourseDetail) {
+  const res = await fetch(`${process.env.API_URL}/courses/${id}`);
   const data = await res.json();
   return data;
 }
