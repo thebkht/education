18457dca5c74f82eeb495011469f24e80258a347
Bakhtiyor Ganijon <me@bkhtdev.com>
2024-07-22 16:43:38 +0500

    refactor: Optimize image loading in InstructorCard, CourseCard, and CourseCard2 components

--- components/home/instructors-section.tsx
+++ components/home/instructors-section.tsx
@@ -1,42 +1,35 @@
 import InstructorCard from "@/components/InstructorCard";
 import { instructors } from "@/data/instructors";
 import { Swiper, SwiperSlide } from "swiper/react";
-import {
-     Navigation,
-     Pagination,
-     A11y,
-     Autoplay
-}
-
-     from "swiper/modules";
+import { Navigation, Pagination, A11y, Autoplay } from "swiper/modules";
 import "swiper/css";
 import { Teacher } from "@/lib/types";
 
 export function InstructorSection({ teachers }: { teachers: Teacher[] }) {
-     return (<>
-          <div className="flex flex-col gap-2 mx-auto container xl:p-0 px-4">
-               <div className="flex-1">
-                    <h3 className="text-2xl font-bold">Bizning o&apos;qituvchilar</h3>
-               </div>
-               <Swiper
-                    modules={[Autoplay, A11y]}
-                    slidesPerView={4}
-                    spaceBetween={24}
-                    className="w-full"
-                    loop
-                    autoplay={{
-                         delay: 2500,
-                         disableOnInteraction: false,
-                    }}
-               >
-                    {
-                         instructors.map((instructor: Teacher, index) => (
-                              <SwiperSlide key={index}>
-                                   <InstructorCard instructor={instructor} />
-                              </SwiperSlide>
-                         ))
-                    }
-               </Swiper>
-          </div>
-     </>)
+  return (
+    <>
+      <div className="container mx-auto flex flex-col gap-2 px-4 xl:p-0">
+        <div className="flex-1">
+          <h3 className="text-2xl font-bold">Bizning o&apos;qituvchilar</h3>
+        </div>
+        <Swiper
+          modules={[Autoplay, A11y]}
+          slidesPerView={4}
+          spaceBetween={24}
+          className="w-full"
+          loop
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+        >
+          {teachers.map((instructor: Teacher, index) => (
+            <SwiperSlide key={index}>
+              <InstructorCard instructor={instructor} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </>
+  );
 }
