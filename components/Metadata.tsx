60d83f9b827879eb595dcdc336ae5026a2026855
Bakhtiyor Ganijon <me@bkhtdev.com>
2024-07-23 12:51:59 +0500

    refactor: Optimize image loading in Metadata component

--- components/Metadata.tsx
+++ components/Metadata.tsx
@@ -1,27 +1,56 @@
 // components/Metadata.js
-import Head from 'next/head';
+import Head from "next/head";
 
-const Metadata = ({ title, description, url, image, ogType }: { title?: string, description?: string, url?: string, image?: string, ogType?: string }) => {
-     const defaultTitle = "Avtomobil yo'llari ilmiy-tadqiqot instituti";
-     const defaultDescription = "Avtomobil yo'llari ilmiy-tadqiqot instituti";
-     const defaultImage = "";
-     const defaultUrl = "https://www.example.com";
+const Metadata = ({
+  title,
+  description,
+  url,
+  image,
+  ogType,
+  noFollow,
+}: {
+  title?: string;
+  description?: string;
+  url?: string;
+  image?: string;
+  ogType?: string;
+  noFollow?: boolean;
+}) => {
+  const defaultTitle = "Avtomobil yo'llari ilmiy-tadqiqot instituti";
+  const defaultDescription = "Avtomobil yo'llari ilmiy-tadqiqot instituti";
+  const defaultImage = "";
+  const defaultUrl = "https://www.example.com";
 
-     return (
-          <Head>
-               <title>{title ? `${title} - ${defaultTitle}` : defaultTitle}</title>
-               <meta name="description" content={description ?? defaultDescription} />
-               <meta property="og:title" content={title ? `${title} - ${defaultTitle}` : defaultTitle} />
-               <meta property="og:description" content={description ?? defaultDescription} />
-               <meta property="og:type" content={ogType ?? "website"} />
-               <meta property="og:url" content={url ?? defaultUrl} />
-               <meta property="og:image" content={image ?? defaultImage} />
-               <meta name="twitter:card" content="summary_large_image" />
-               <meta name="twitter:title" content={title ?? defaultTitle} />
-               <meta name="twitter:description" content={description ?? defaultDescription} />
-               <meta name="twitter:image" content={image ?? defaultImage} />
-          </Head>
-     );
+  return (
+    <Head>
+      <title>{title ? `${title} - ${defaultTitle}` : defaultTitle}</title>
+      <meta name="description" content={description ?? defaultDescription} />
+      <meta
+        property="og:title"
+        content={title ? `${title} - ${defaultTitle}` : defaultTitle}
+      />
+      <meta
+        property="og:description"
+        content={description ?? defaultDescription}
+      />
+      <meta property="og:type" content={ogType ?? "website"} />
+      <meta property="og:url" content={url ?? defaultUrl} />
+      <meta property="og:image" content={image ?? defaultImage} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title ?? defaultTitle} />
+      <meta
+        name="twitter:description"
+        content={description ?? defaultDescription}
+      />
+      <meta name="twitter:image" content={image ?? defaultImage} />
+      {noFollow && (
+        <>
+          <meta name="googlebot" content="noindex, nofollow" />
+          <meta name="robots" content="noindex, nofollow" />
+        </>
+      )}
+    </Head>
+  );
 };
 
 export default Metadata;
