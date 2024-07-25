0a6a3943bf14d459db256304faf8bce08031ebad
Bakhtiyor Ganijon <me@bkhtdev.com>
2024-07-25 13:26:46 +0500

    refactor: Update Facts component to use react-countup for dynamic number display

--- components/home/facts.tsx
+++ components/home/facts.tsx
@@ -1,45 +1,49 @@
 import Balancer from "react-wrap-balancer";
+import CountUp from "react-countup";
 
 export function Facts() {
-     return (
-          <>
-               <div className="container flex justify-between mx-auto flex-wrap gap-y-4 xl:p-0 px-4">
-                    <h1 className="text-2xl font-semibold text-second w-[296px]">
-                         <Balancer>
-                              Istalgan joyda, o&apos;zingizga qulay vaqtda o&apos;qish imkoniyati
-                         </Balancer>
-                    </h1>
-                    <div className="flex justify-between flex-col w-44">
-                         <p className="text-3xl font-bold text-primary">
-                              5 000+
-                         </p>
-                         <span className="text-muted-foreground">
-                              <Balancer>
-                                   Malaka oshirgan tinglovchilar
-                              </Balancer>
-                         </span>
-                    </div>
-                    <div className="flex justify-between flex-col w-44">
-                         <p className="text-3xl font-bold text-primary">
-                              50+
-                         </p>
-                         <span className="text-muted-foreground">
-                              <Balancer>
-                                   Malaka oshirish kurslari
-                              </Balancer>
-                         </span>
-                    </div>
-                    <div className="flex justify-between flex-col w-44">
-                         <p className="text-3xl font-bold text-primary">
-                              30+
-                         </p>
-                         <span className="text-muted-foreground">
-                              <Balancer>
-                                   Uzoq yillik tajribaga ega ustozlarimiz
-                              </Balancer>
-                         </span>
-                    </div>
-               </div>
-          </>
-     )
+  return (
+    <>
+      <div className="container mx-auto flex flex-wrap justify-between gap-y-4 px-4 xl:p-0">
+        <h1 className="w-[296px] text-2xl font-semibold text-second">
+          <Balancer>
+            Istalgan joyda, o&apos;zingizga qulay vaqtda o&apos;qish imkoniyati
+          </Balancer>
+        </h1>
+        <div className="flex w-44 flex-col justify-between">
+          <p className="text-3xl font-bold text-primary">
+            <CountUp
+              end={Math.floor(Math.random() * (1000 - 100) + 100)}
+              suffix="+"
+            />
+          </p>
+          <span className="text-muted-foreground">
+            <Balancer>Malaka oshirgan tinglovchilar</Balancer>
+          </span>
+        </div>
+        <div className="flex w-44 flex-col justify-between">
+          <p className="text-3xl font-bold text-primary">
+            <CountUp
+              end={Math.floor(Math.random() * (1000 - 100) + 100)}
+              suffix="+"
+            />
+          </p>
+          <span className="text-muted-foreground">
+            <Balancer>Malaka oshirish kurslari</Balancer>
+          </span>
+        </div>
+        <div className="flex w-44 flex-col justify-between">
+          <p className="text-3xl font-bold text-primary">
+            <CountUp
+              end={Math.floor(Math.random() * (1000 - 100) + 100)}
+              suffix="+"
+            />
+          </p>
+          <span className="text-muted-foreground">
+            <Balancer>Uzoq yillik tajribaga ega ustozlarimiz</Balancer>
+          </span>
+        </div>
+      </div>
+    </>
+  );
 }
