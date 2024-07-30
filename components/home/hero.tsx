d8e2af9e31668ef460e9c04212b7eabd357af3fc
Bakhtiyor Ganijon <me@bkhtdev.com>
2024-07-30 10:47:56 +0500

    refactor: Improve Hero component by updating container class and fixing import statement

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -1,11 +1,11 @@
-import HeroCarousel from '@/components/HeroCarousel'
+import HeroCarousel from "@/components/HeroCarousel";
 
 export function Hero() {
-     return (
-          <>
-               <div className="container md:mx-auto mb-8 px-4 xl:px-0">
-                    <HeroCarousel />
-               </div>
-          </>
-     )
+  return (
+    <>
+      <div className="container mb-8 md:mx-auto xl:px-0">
+        <HeroCarousel />
+      </div>
+    </>
+  );
 }
