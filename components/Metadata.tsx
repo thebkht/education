19387392e0bfde0857e7679aaa1053fea06df9d7
Bakhtiyor Ganijon <me@bkhtdev.com>
2024-07-31 16:31:18 +0500

    refactor: Optimize image loading in CourseLayout, CourseContent, LecturePage, CourseCard, and CourseCard2 components

--- components/Metadata.tsx
+++ components/Metadata.tsx
@@ -49,6 +49,29 @@ const Metadata = ({
           <meta name="robots" content="noindex, nofollow" />
         </>
       )}
+      <meta
+        name="description"
+        content="Avtomobil yoʻllari ilmiy tadqiqot instituti"
+      />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/images/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/images/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/images/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
     </Head>
   );
 };
