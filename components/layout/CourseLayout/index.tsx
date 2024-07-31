d540805e8f69d73faed0de234e8640825c4d2dc6
Bakhtiyor Ganijon <me@bkhtdev.com>
2024-07-31 16:24:20 +0500

    refactor: Optimize image loading in CourseLayout component

--- components/layout/CourseLayout/index.tsx
+++ components/layout/CourseLayout/index.tsx
@@ -4,11 +4,12 @@ import Link from "next/link";
 import Image from "next/image";
 import styles from "./styles.module.css";
 import { cn } from "@/lib/utils";
-import React from "react";
+import React, { useEffect } from "react";
 import { Inter } from "next/font/google";
 import { getLocalStorage, setLocalStorage } from "@/helpers/localStorage";
 import { IUser } from "@/interfaces/auth";
 import { Toaster } from "sonner";
+import { Skeleton } from "@/components/Skeleton";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,9 +23,21 @@ export default function Index({
   user: IUser;
 }) {
   const [collapsed, setCollapsed] = React.useState<boolean>(false);
+  const [loading, setLoading] = React.useState<boolean>(true);
+
+  useEffect(() => {
+    const localCollapsed = getLocalStorage("collapsed");
+    if (localCollapsed) {
+      setCollapsed(localCollapsed);
+    }
+    setLoading(false);
+  }, []);
 
   const handleCollapse = () => {
-    setCollapsed(!collapsed);
+    setCollapsed((prev) => {
+      setLocalStorage(!prev ? 1 : 0, "collapsed");
+      return !prev;
+    });
   };
   return (
     <>
@@ -35,33 +48,45 @@ export default function Index({
           inter.className,
         )}
       >
-        <div className="z-50 flex">
-          <div
-            className={`flex-[1_1_1px] ${collapsed ? "w-[72px]" : "w-[250px]"} fixed z-50 box-border flex h-14 flex-col justify-center border-r border-border px-4`}
-          >
-            <Link
-              href="/"
-              className={
-                "relative flex cursor-pointer select-none rounded-lg border-none"
-              }
+        {!loading ? (
+          <div className="z-50 flex">
+            <div
+              className={`flex-[1_1_1px] ${collapsed ? "w-[72px]" : "w-[250px]"} fixed z-50 box-border flex h-14 flex-col justify-center border-r border-border px-4`}
             >
-              <Image
-                src={collapsed ? "/favicon.ico" : "/logo.png"}
-                alt="Logo"
-                width={141}
-                height={32}
-                className={`h-auto transition-all duration-300 ease-in-out ${collapsed ? "hidden" : ""}`}
-                style={{ height: "auto" }}
-              />
-            </Link>
+              <Link
+                href="/"
+                className={
+                  "relative flex cursor-pointer select-none rounded-lg border-none"
+                }
+              >
+                <Image
+                  src={collapsed ? "/favicon.ico" : "/logo.png"}
+                  alt="Logo"
+                  width={141}
+                  height={32}
+                  className={`h-auto transition-all duration-300 ease-in-out ${collapsed ? "hidden" : ""}`}
+                  style={{ height: "auto" }}
+                />
+              </Link>
+            </div>
           </div>
-        </div>
-        <Header
-          collapsed={collapsed}
-          onCollapse={handleCollapse}
-          session={user}
-        />
-        <Sidebar collapsed={collapsed} />
+        ) : (
+          <Skeleton className="w-full" />
+        )}
+        {!loading ? (
+          <Header
+            collapsed={collapsed}
+            onCollapse={handleCollapse}
+            session={user}
+          />
+        ) : (
+          <Skeleton className="w-full px-6" />
+        )}
+        {!loading ? (
+          <Sidebar collapsed={collapsed} />
+        ) : (
+          <Skeleton className="w-full px-4" count={5} />
+        )}
         <main className="relative z-0 flex min-h-0 flex-[1_0_auto] justify-center bg-accent">
           <div className="flex w-full flex-[1_0_auto]">
             <div className="flex flex-1">
